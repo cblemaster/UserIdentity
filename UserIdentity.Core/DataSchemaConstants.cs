1bc88b883c5fb3d9b975bf8d7c828b3e5cf5cbdc
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-09 12:19:10 -0400

    added data schema constants to core project

---
+++ UserIdentity.Core/DataSchemaConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace UserIdentity.Core;
+
+public static class DataSchemaConstants
+{
+    public const int USER_USERNAME_MAX_LENGTH = 50;
+    public const int USER_PASSWORD_MAX_LENGTH = 50;
+    public const int USER_FIRSTNAME_MAX_LENGTH = 255;
+    public const int USER_LASTNAME_MAX_LENGTH = 255;
+    public const int USER_EMAIL_MAX_LENGTH = 255;
+    public const int USER_PHONE_MAX_LENGTH = 10;
+    public const int USER_PASSWORDHASH_MAX_LENGTH = 200;
+    public const int USER_SALT_MAX_LENGTH = 200;
+    public const int ROLE_ROLE_MAX_LENGTH = 50;
+    public const int REQUIRED_STRING_MIN_LENGTH = 1;
+}

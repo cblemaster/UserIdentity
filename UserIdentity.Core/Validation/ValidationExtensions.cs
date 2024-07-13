a3760183120746a80ac5ba8130afe8cf962720b4
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-13 00:09:38 -0400

    added validation extension methods in core project

---
+++ UserIdentity.Core/Validation/ValidationExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace UserIdentity.Core.Validation;
+
+internal static class ValidationExtensions
+{
+    internal static string GetErrorMessageForIsRequiredAndHasMaxLength
+        (this string propName, int maxLength) =>
+            $"{propName} is required and must be {maxLength} " +
+            "characters or fewer.";
+}

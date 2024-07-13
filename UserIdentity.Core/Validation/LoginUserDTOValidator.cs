e4053c5760f19ffbe2b78596ca77ba62cb56f7e9
Brian LeMaster <brian.lemaster@outlook.com>
2024-07-13 00:08:47 -0400

    added login user dto in core project

---
+++ UserIdentity.Core/Validation/LoginUserDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using UserIdentity.Core.DataTransferObjects;
+
+namespace UserIdentity.Core.Validation;
+
+public class LoginUserDTOValidator : AbstractValidator<LoginUserDTO>
+{
+    public LoginUserDTOValidator()
+    {
+        RuleFor(v => v.Username.Length)
+            .InclusiveBetween(DataSchemaConstants.REQUIRED_STRING_MIN_LENGTH, DataSchemaConstants.USER_USERNAME_MAX_LENGTH)
+            .WithMessage(nameof(Core.Entities.User.Username).GetErrorMessageForIsRequiredAndHasMaxLength(DataSchemaConstants.USER_USERNAME_MAX_LENGTH));
+        RuleFor(v => v.Password.Length)
+            .InclusiveBetween(DataSchemaConstants.REQUIRED_STRING_MIN_LENGTH,
+                DataSchemaConstants.USER_PASSWORD_MAX_LENGTH)
+            .WithMessage(nameof(Core.DataTransferObjects.CreateUserDTO.Username).GetErrorMessageForIsRequiredAndHasMaxLength(DataSchemaConstants.USER_PASSWORD_MAX_LENGTH));
+    }
+}
